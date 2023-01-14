08bbed0ccbf9e51bc99ccd60e0adba0ee5214029
adithyan-r1281 <adithyanr12801@gmail.com>
2023-01-14 12:11:05 +0530

    Todo_List in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoitem:string='';
+
+  constructor(){
+    // this.SystemDate=new Date;
+
+  }
+  // SystemDate:any;
   title = 'mytodo';
+  tasks:any=[]
+  newtask(content:any){
+
+    if(content!=''){
+      this.tasks.push({id:this.tasks.length,title:content.value})
+      // console.log(this.tasks);
+      
+    }
+    content.value='';
+  }
+  removetask(id:number){
+    this.tasks=this.tasks.filter((item:{id:number})=>item.id!=id)
+  }
+  removeall(){
+    this.tasks=[]
+  }
+
 }
